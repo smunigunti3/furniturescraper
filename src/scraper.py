b435a4d1f5ec7ba4a85ab63753d50f601d6ede04
smunigunti3 <msiri006@gmail.com>
2025-03-29 16:22:55 -0400

    allSofas
    Creates a list with links to all sofas on Ikea

--- src/scraper.py
+++ src/scraper.py
@@ -1,36 +1,7 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
-<<<<<<< Updated upstream
 import time
-
-# Set up WebDriver
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-
-# Visit the URL
-url = "https://megafurniture.us/collections/sofas"
-driver.get(url)
-
-# Give the page a little time to load
-time.sleep(3)
-
-try:
-    # Find all product names using the 'card__title' class
-    product_elements = driver.find_elements(By.CLASS_NAME, "card__title")
-
-    # Print out each product name
-    for product in product_elements:
-        print(product.text)
-
-except Exception as e:
-    print("Error occurred:", e)
-
-finally:
-    driver.quit()
-=======
 from selenium.webdriver import ActionChains
-import time
 
 driver = webdriver.Chrome()
 driver.get("https://www.ikea.com/us/en/cat/sofas-sectionals-fu003/")
@@ -67,8 +38,7 @@ for element in results2:
     product = product_element.get_attribute('href')
     productlist.append(product)
 
-# for product in productlist:
-#     print(product)
+for product in productlist:
+    print(product)
 
 driver.quit()
->>>>>>> Stashed changes
